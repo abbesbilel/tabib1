ccebd1874c142e36a4ec6cba8ab5bf3d4b0dda37
abbesbilel <b_abbes@estin.dz>
2025-01-25 23:22:11 +0100

    redone fixed routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import './App.css';
 
-import Page from './components/Page';
-
-
-// Font Awesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Appointments from './components/Appointments';
+import Settings from './components/Settings';
+import SideBar from './components/SideBar';
+import Head from './components/Head';
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
-    <div className="App">
-      <Page />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div className="page d-flex">
+          {/* Sidebar */}
+          <SideBar />
+          {/* Main Content */}
+          <div className="content w-full">
+            {/* Header */}
+            <Head />
+            {/* Routes */}
+            <Routes>
+              <Route
+                path="/"
+                element={<Wrapper />}
+              />
+              <Route path="appointments" element={<Appointments />} />
+              <Route path="settings" element={<Settings />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
