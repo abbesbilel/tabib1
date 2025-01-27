e384216868e4b484c53420f28c4bd67518005a45
abbesbilel <b_abbes@estin.dz>
2025-01-27 06:07:59 +0100

    phase 1 selection responsive done

--- src/page/Selection/components/SeleCtion.jsx
+++ src/page/Selection/components/SeleCtion.jsx
@@ -4,6 +4,7 @@ import faceb from '../../../assets/face.png'
 import wattsup from '../../../assets/whatsapp.png'
 import select from '../../../assets/selection.png'
 import DoctorGrid from './DoctorGrid'
+import Footer from '../../../components/Footer'
 
 
 const SeleCtion = () => {
@@ -11,10 +12,10 @@ const SeleCtion = () => {
     return (
         <div className='app-container'>
             <header className="header-appoint">
-                <div className="logo">
+                {/* <div className="logo">
                     <span className="logo-dz">DZ</span>
                     <span className="logo-tabib">TABIB</span>
-                </div>
+                </div> */}
                 <div className="search-bar">
                     <input
                         type="text"
@@ -60,44 +61,7 @@ const SeleCtion = () => {
             </div>
 
 
-            <footer className="footer">
-                <div className="footer-content">
-                    <div className="footer-section">
-                        <div className="logo">
-                            <span className="logo-dz">DZ</span>
-                            <span className="logo-tabib">TABIB</span>
-                        </div>
-                        <div className='place'>8 W. South St.Buford, GA 30518</div>
-                        <div className='place'>5Briarwood LaneVienna, VA 22180 RER</div>
-                        <div className="social-icons">
-                            <img className='facebook' src={faceb} alt="facebook" />
-                            <img className='wattsup' src={wattsup} alt="google" />
-                        </div>
-                    </div>
-
-                    <div className="footer-section">
-                        <a href="#">Home</a>
-                        <a href="#">Services</a>
-                        <a href="#">Our team</a>
-                        <a href="#">Testimonials</a>
-                    </div>
-
-                    <div className="footer-section">
-                        <h3>Book Now</h3>
-                        <a href="#">Appointment</a>
-                        <a href="#">Teleconsultation</a>
-                    </div>
-
-                    <div className="footer-section">
-                        <h3>Contact</h3>
-                        <p>DZTABIB@mail.com</p>
-                        <p>Phone: +213512345678</p>
-                    </div>
-                </div>
-                <div className="footer-bottom">
-                    © all rights reserved 2024
-                </div>
-            </footer>
+           <Footer/>
         </div>
     )
 }
