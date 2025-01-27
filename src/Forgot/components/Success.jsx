c8baa9e588bae5c2efac6a7a99dc6fb3b13dc324
abbesbilel <b_abbes@estin.dz>
2025-01-27 01:07:35 +0100

    fixed forgot password route

--- src/Forgot/components/Success.jsx
+++ src/Forgot/components/Success.jsx
@@ -3,7 +3,7 @@ import sadDoctorImage from './assets/sad_doctor.png';
 import logoImage from './assets/DZTABIB.png';
 import check from './assets/upload.svg'
 import './Success.css'
-import { Link } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 export default function Success() {
   return (
@@ -22,7 +22,9 @@ export default function Success() {
                 <p className='p-suc'>Your password has been reset successfully</p>
                 
                 </div>
+                <Link to="/" style={{width: '100%'}}>
                 <button className='Next'>Next</button>
+                </Link>
                 
                 </div>
         </div>
