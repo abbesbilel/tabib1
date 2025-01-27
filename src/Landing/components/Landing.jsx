347c22a9afc55953c35877792fe7914e55985090
abbesbilel <b_abbes@estin.dz>
2025-01-27 01:34:05 +0100

    little text fix

--- src/Landing/components/Landing.jsx
+++ src/Landing/components/Landing.jsx
@@ -7,7 +7,7 @@ export default function Landing() {
         <div className='Landing-container'>
             <div className="text-container">
                 <div className="text">
-                <p className='move-title'>Booking apointment online</p>
+                <p className='move-title'>Booking Appointment Online</p>
                 <h1>Many Doctors in several feilds At your service</h1>
                 <p className='caption-text'>Many Doctors in various fields are at your service, ready to provide personalized care tailored to your needs. Whether you're seeking a routine check-up, specialized treatment, or expert advice, our team of experienced professionals is here to help. With just a few clicks, you can easily schedule your visit and take the first step toward better health.</p>
                 </div>
